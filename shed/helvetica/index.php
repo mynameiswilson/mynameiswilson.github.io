b8d429d14126cac3d965455b1904f62500ce0449
ben <ben@9a4b3800-4c9c-4e24-ae1c-011c3f90af91>
2013-09-10 00:25:16 +0000

    cleanup commit
    git-svn-id: http://svn.kygeek.org/ben/benwilson.org@95 9a4b3800-4c9c-4e24-ae1c-011c3f90af91

---
+++ shed/helvetica/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<head>
+<title>benwilson.org | a study in helvetica</title>
+<style type="text/css">
+@import url('reset.css');
+body { 
+	font: normal normal 1em/1em Helvetica, sans-serif;
+}
+
+h1 { 
+	font-size: 5em; 
+	line-height: 1.5em;
+	font-weight: bold;
+} 
+
+h2 { 
+	font-size: 2em;
+	line-height: 1.5em;
+	border-bottom: 1px solid #f00 
+}
+
+</style>
+</head>
+<body>
+<h2>pro web developer | cyclist | dad</h2>
+<h1>Ben Wilson</h1>
+
+</body>
+</html>
