271514e57b2ba40fd0f8205e44131e1481b11633
Ben Wilson <ben@thelocust.org>
2016-01-20 16:08:08 -0500

    added hash to secret key

--- deploy.php
+++ deploy.php
@@ -1,7 +1,8 @@
 <?php 
 
-if (isset($_GET['secret']) && $_GET['secret'] == 'some-secret-key') {
+if (isset($_GET['secret']) && hash("sha256",$_GET['secret'] == '766cf657353421f70937fd1cbf22412d92997d02a15bb755c1a21090776de189') {
     // @todo integrate a sanity check against this:
     //       https://help.github.com/articles/what-ip-addresses-does-github-use-that-i-should-whitelist/
     `git pull`;  
 }
+?>
